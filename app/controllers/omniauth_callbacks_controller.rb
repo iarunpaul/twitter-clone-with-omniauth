2dbefd0f47ec41e3936198ef5390431fc22e305a
iarunpaul <iarunpaul@yahoo.com>
2021-07-27 22:18:52 +0530

    Added validations for creating and updating users both ways

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,9 +2,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.find_from_auth_hash(auth_hash)
     if user.persisted?
-      flash[:success] = "Signed in as #{user.name}"
+      flash.notice = "Signed in as #{user.name}"
       sign_in_and_redirect user
     else
+      session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_path, alert: 'Error occured'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,43 @@ class User < ApplicationRecord
   has_many :tweeets
   # validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 50 }
   # validates :name, presence: true, length: { minimum: 4, maximum: 20 }
-  validates :email, :password, presence: true, unless: :provider?
-  validates :email, uniqueness: { case_sensitive: false }, length: {maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
-  validates :password, length: {minimum: 6}, confirmation: :required, unless: :provider?
+  validates :email, presence: true#, unless: :provider?
+  validates :password, presence: true, length: {minimum: 6}, confirmation: :required, on: :create, unless: :provider?
+  validates :email, uniqueness: { case_sensitive: false }, length: {maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: {minimum: 6}, confirmation: :required, allow_blank: true, on: :update#, unless: :provider?
+
+def password_required?
+  provider.blank? || !password.nil? || !password_confirmation.nil?
+end
+
 
   def self.find_from_auth_hash(auth_hash)
-    byebug
+    # byebug
     user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
       user.provider = auth_hash.provider
       user.uid = auth_hash.uid
       user.username = auth_hash.info.nickname
       user.name = auth_hash.info.name
+      user.email = auth_hash.info.email
     end
   end
 
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"]) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 end
